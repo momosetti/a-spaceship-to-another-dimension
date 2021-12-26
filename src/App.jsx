036abcbc4657bdaa46a9a1a0fd269c029a98ccfe
Momo Setti <zinasetti@gmail.com>
2021-12-26 02:45:22 +0100

    feat: implement `planet` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Container from "./components/layout/container";
 import MainWrapper from "./components/layout/mainWrapper";
-import Map from "./components/map/map";
 import ButtonsPad from "./features/control";
 import { useSelector } from "react-redux";
 import { resetVehicleObstacleStatus } from "./features/KF96/KF96Slice";
 import ObstacleReporter from "./components/obstacleReporter";
+import Planet from "./features/planet/planet";
 
 export default function App() {
   const { planet, vehicleCurrentPosition, vehicleObstacle } = useSelector(
@@ -19,7 +19,7 @@ export default function App() {
           obstacleNumber={vehicleObstacle.obstacleNumber}
           resetterAction={resetVehicleObstacleStatus}
         />
-        <Map planetGridSchema={planet} />
+        <Planet planet={planet} />
         <ButtonsPad vehicleCurrentPosition={vehicleCurrentPosition} />
       </MainWrapper>
     </Container>
--- src/features/planet/planet.jsx
+++ src/features/planet/planet.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
-export default function Planet() {
-  return <div></div>;
+import Map from "../../components/map/map";
+import PropTypes from "prop-types";
+export default function Planet({ planet }) {
+  return <Map planetGridSchema={planet} />;
 }
+Planet.propTypes = {
+  planet: PropTypes.object.isRequired,
+};
